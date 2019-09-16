a82cc6a66901926b13be8a011205a3b4f2fcc23a
JAESEO LEE <goodstart57@gmail.com>
2019-09-17 00:18:53 +0900

    EDIT crawler => collector 크롤러 이름 변경 & EDIT 기사 내용 수집 기능 추가

--- collector/__init__.py
+++ collector/__init__.py
@@ -25,7 +25,7 @@ class UrlManager:
             return urljoin(self.domain, *path)
         else:
             return urljoin(self.domain, path)
-        
+
 
 class NewsCollector:
     def __init__(self):
@@ -33,6 +33,9 @@ class NewsCollector:
         self.articles = {}
         self.today = datetime.now()
 
+    ##########
+    #scraping#
+    ##########
     def get_html(self, url):
         res = requests.get(url)
 
@@ -50,6 +53,9 @@ class NewsCollector:
     def get_elements_by_css(self, bs_page, css_selector):
         return bs_page.select(css_selector)
     
+    ######
+    #util#
+    ######
     def format_date(self, dt):
         if re.search("분 전$", dt):
             return self.today - timedelta(minutes=int(dt[:-3]))
@@ -60,6 +66,9 @@ class NewsCollector:
         else:
             return datetime.strptime(dt, "%Y.%m.%d")
     
+    ######
+    #main#
+    ######
     def collect_latest(self, path="main"):
         """최신 기사 크롤링"""
         url = self.UrlManager.get(path, is_alias=True)
@@ -80,6 +89,7 @@ class NewsCollector:
             self.articles['latest'].append({
                 'date': date.strftime("%Y-%m-%d"),
                 'title': a.text,
+                'content': self.collect_news_content(a.attrs.get('href')),
                 'url': a.attrs.get('href'),
             })
     
@@ -111,10 +121,60 @@ class NewsCollector:
 
             self.articles['main'].append({
                 'title': title,
-                'content': '',
+                'content': self.collect_news_content(url),
                 'themes': themes,
                 'date': self.format_date(dt).strftime("%Y-%m-%dT%H:%M:%S"),
                 'url': url
-
             })
     
+    def collect_news_content(self, news_url):
+        # url 예외상황 처리
+        news_url = self.UrlManager.get(news_url)
+        # 변수 설정
+        detail_page_num = 0
+        is_one_page = False
+        content = ""
+
+        while True:
+            detail_res = self.get_html(news_url + f"?page=0,{detail_page_num}")
+            detail_page_num += 1
+            detail_bs = self.parse_response_to_bs(detail_res)
+
+            # 첫번째 pagination 제거
+            pagination = self.get_element_by_css(detail_bs, ".pagination.pagination")
+
+            # pagination 존재 == 2장 이상 뉴스
+            if pagination:
+                pagination.extract()
+
+                # 두번째 안보이는 pagination에서 마지막 페이지 정보 얻기
+                pagination = self.get_element_by_css(detail_bs, ".pagination.pagination").extract()
+                pagination = self.get_elements_by_css(pagination, "li")
+
+                # 페이지 넘기면 break
+                if detail_page_num == len(pagination):
+                    break
+            # pagination 없는 한페이지 뉴스
+            else:
+                is_one_page = True
+
+            # 기사 내용 추출 (html 형태)
+            detail_content = str(self.get_element_by_css(detail_bs, ".node_body.cb"))
+            # 전처리
+            detail_content = detail_content.strip()
+            detail_content = detail_content.replace("\n", "<br>")
+            detail_content = detail_content.replace("\xa0", "")
+            detail_content = detail_content.replace('src="', 'src="http://www.itworld.co.kr/')
+            detail_content = detail_content.replace("<br/>", "")
+            detail_content = detail_content.replace("\t", "")
+            detail_content = detail_content.replace("\r", "")
+
+            # 최종 변수에 담기
+            content += detail_content
+
+            # pagination 없는 한페이지
+            if is_one_page:
+                break
+        
+        return content
+        
