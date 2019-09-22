11a74d07b6e0d5fc5039acde5f9db860301037a0
JAESEO LEE <goodstart57@gmail.com>
2019-09-22 14:38:15 +0900

    EDIT> api.news.models
    change url data type, add created_at, updated_at to ref Tables

--- api/news/models.py
+++ api/news/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class Theme(models.Model):
     name = models.CharField(max_length=20, default='')
-    url = models.IntegerField(blank=True)
+    url = models.CharField(max_length=100, unique=True, blank=False)
     following_themes = models.ManyToManyField('self', related_name='followed_themes', blank=True)
 
     __str__ = lambda self: self.name
@@ -15,8 +15,8 @@ class Article(models.Model):
     title = models.CharField(max_length=100, default='')
     content = models.TextField(default='')
     themes = models.ManyToManyField(Theme, related_name='theme_articles', blank=True)
-    date = models.DateTimeField(default=datetime.now())
-    url = models.IntegerField(blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    url = models.CharField(max_length=100, unique=True, blank=False)
     # timestamp
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -26,11 +26,17 @@ class Article(models.Model):
 
 class Latest(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     __str__ = lambda self: self.article.title
 
 class Main(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     __str__ = lambda self: self.article.title
     
